74a66f23f44de35f33e7b4ee17fef0a0ef6b03d5
Renato Veneroso <rveneroso@gmail.com>
2024-03-31 17:16:40 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>This is where you must put your components as they'll be loaded by React engine.</h1>
+      <h2>Music is enough for a lifetime but a lifetime is not enough for music.</h2>
     </div>
-  );
+  )
 }
-
-export default App;
