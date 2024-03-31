ed2eedb81c0c95458a47811ed128bfa8d5cf843a
Renato Veneroso <rveneroso@gmail.com>
2024-03-31 19:22:25 -0300

    Introdução à Class Component.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,36 @@
-export default function App() {
+import React, { Component} from "react";
 
-  const Equipe = (props) => {
+class Equipe extends Component {
+  render(){
     return(
       <div>
-        <Sobre nome = {props.nome} cargo = {props.cargo} idade = {props.idade}/>
-        <Social fb={props.facebook}/>
+        <Sobre nome={this.props.nome} cargo={this.props.cargo} idade={this.props.idade} />
         <hr/>
       </div>
     );
   }
+}
 
-  const Sobre = (props) => {
+class Sobre extends Component {
+  render(){
     return(
       <div>
-        <h2>Olá, eu sou o(a) {props.nome}</h2>
-        <h3>Cargo: {props.cargo}</h3>
-        <h3>Idade: {props.idade} anos</h3>
-      </div>
-    );
-  } 
-
-  const Social = (props) => {
-    return(
-      <div>
-        <a href={props.fb}>Facebook</a>
-        <a>LinkedIn: </a>
-        <a>YouTube: </a>
+        <h2>Olá! Sou o  {this.props.nome} </h2>
+        <h3>Cargo: {this.props.cargo} </h3>
+        <h3>Idade: {this.props.idade} anos</h3>
       </div>
     );
   }
+}
+
+export default function App() {
 
   return (
     <div>
       <h1>Conheça nossa equipe!</h1>
-      <Equipe nome="Lucas" cargo="Programador" idade="29" 
-              facebook="https://google.com/"/>
-      <Equipe nome="Matheus" cargo="Designer" idade="19" />
+      <Equipe nome="Ludwig van Beethoven" cargo="Pianista & compositor" idade="253"/>
+      <Equipe nome="Nicolo Paganini" cargo="Violinista & compositor" idade="241"/>
+      <Equipe nome="Camille Saint-Saens" cargo="Organista & compositor" idade="188"/>
     </div>
   )
 }
